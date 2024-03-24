dd3025ea48ba27e74dfcd3fb51dfdf15cc07af4f
Felipe marcon <felipemarcon357@gmail.com>
2024-03-24 14:13:30 -0300

    INICIANDO REPOSITORIO

---
+++ Nova pasta/script.js
@@ -0,0 +1,79 @@
+let count = 1;
+document.getElementById('radio1').checked = true;
+
+setInterval(function () {
+    nextImage()
+}, 4000);
+
+function nextImage() {
+    count++;
+
+    if (count > 3) {
+        count = 1;
+    }
+
+    document.getElementById('radio' + count).checked = true;
+}
+
+let cont = 1;
+function nextImagetenis() {
+
+    cont++;
+    if (cont > 5) {
+        cont = 1;
+    }
+
+    document.getElementById('radio' + cont + 't').checked = true;
+}
+
+function backImagetenis() {
+
+    cont--;
+    if (cont < 1) {
+        cont = 5;
+    }
+
+    document.getElementById('radio' + cont + 't').checked = true;
+}
+
+let contar = 1;
+function nextImagetenisd() {
+
+    contar++;
+    if (contar > 5) {
+        contar = 1;
+    }
+
+    document.getElementById('radio' + contar + 'td').checked = true;
+}
+
+function backImagetenisd() {
+
+    contar--;
+    if (contar < 1) {
+        contar = 5;
+    }
+
+    document.getElementById('radio' + contar + 'td').checked = true;
+}
+
+let contarParaRoupas = 1;
+function nextImageRoupas() {
+
+    contarParaRoupas++;
+    if (contarParaRoupas > 3) {
+        contarParaRoupas = 1;
+    }
+
+    document.getElementById('radio' + contarParaRoupas + 'r').checked = true;
+}
+
+function backImageRoupas() {
+
+    contarParaRoupas--;
+    if (contarParaRoupas < 1) {
+        contarParaRoupas = 3;
+    }
+
+    document.getElementById('radio' + contarParaRoupas + 'r').checked = true;
+}
